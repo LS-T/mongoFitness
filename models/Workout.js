801065daa6f64df2324133684cddab74173bd8f1
LS-T <leo.s.teruel@gmail.com>
2021-05-07 17:55:18 -0400

    get route for workouts functioning

--- models/Workout.js
+++ models/Workout.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-
+// Create workout collection
 const WorkoutSchema = new Schema({
   day: {
       type: Date,
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,26 @@
 const router = require("express").Router();
+const { Workout } = require("../models");
+const db = require("../models");
+
+// get workouts 
+
+router.get('/api/workouts', (req,res) => {
+    Workout.aggregate ([
+        {
+            // add new field totalDuration to workout collection, equate it to the sum of exercise.duration
+            $addFields: {
+                totalDuration: { $sum: '$exercises.duration'},
+            },
+
+        }
+    ])
+        .then((previousWorkouts) => {
+            res.json(previousWorkouts);
+        })
+        .catch((err) => res.json(err));
+});
+
+
 
 
 module.exports = router;
