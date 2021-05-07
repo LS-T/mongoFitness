7dfd34e80eec179de3d985d03e149f8f0fa65ec0
LS-T <leo.s.teruel@gmail.com>
2021-05-07 15:51:39 -0400

    setup schema and fields needed

---
+++ models/Workout.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  day: {
+      type: Date,
+      default: Date.now
+  },
+  exercises: [
+      {
+        type: {
+            type: String,
+            trim: true,
+          },
+      
+          name: {
+              type: String,
+              trim: true,
+          },
+      
+          duration: Number,
+      
+          weight:Number,
+      
+          distance: Number,
+      
+          reps: Number,
+      
+          sets: Number
+      }
+  ]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.use(morgan("dev"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/htmlRoutes"));
+app.use(require("./routes/apiRoutes"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
